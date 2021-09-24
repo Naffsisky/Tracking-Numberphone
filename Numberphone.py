e4d25b74e1f14d07fb66c60fe4f8719d2c06500e
Nap <70518391+Naffsisky@users.noreply.github.com>
2021-09-24 22:19:59 +0700

    Update Numberphone.py

--- Numberphone.py
+++ Numberphone.py
@@ -1,13 +1,14 @@
+#module
 import phonenumbers
 from phonenumbers import timezone,geocoder,carrier
-
+#userinput
 user = (input('Masukan nomer telepon ex: +62 : ')) 
-
+#data
 phonenumber = phonenumbers.parse(user)
 timeZone = timezone.time_zones_for_number(phonenumber)
 Carrier = carrier.name_for_number(phonenumber,'id')
 Region = geocoder.description_for_number(phonenumber,'id')
-
+#output
 print(phonenumber)
 print('Asal Nomer = ', timeZone)
 print('Provider = ', Carrier)
